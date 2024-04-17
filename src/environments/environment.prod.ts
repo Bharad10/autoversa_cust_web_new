7ced9d490018d931e415c48409f7889915f1ee4c
neerav-vinod <neeravvinod2k18@gmail.com>
2024-04-17 10:51:53 +0530

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,9 @@
 // version code:0.0.4
 export const environment = {
   production: true,
-  //base_url:"https://autoversa.com/autoversa_api/",
+  base_url:"https://autoversa.com/autoversa_api/",
   //base_url:"https://autoversa.com/maraghiapp_api/",
-  base_url: "http://localhost:8080/",
+  // base_url: "http://localhost:8080/",
   SOCKET_ENDPOINT: 'https://chat-server.autoversa.com/',
   nm_url:'http://nasermohsin.fortidyndns.com:35146/nm_spare_fetch/index.php/',
   aws_url:"https://autoversa-media.s3.me-central-1.amazonaws.com/"
